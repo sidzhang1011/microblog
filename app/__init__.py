189121c5c2bfe185f01a983638b87b272eca3034
Sid Zhang <zsg@outlook.com>
2020-08-11 11:41:41 +0800

    add templates

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from config import Config
 
 flask_app = Flask(__name__)
+flask_app.config.from_object(Config)
+print(flask_app.config)
 
 from app import routes
 
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import flask_app
+from app.forms import LoginForm
 
 count = 0
 
@@ -21,3 +22,9 @@ def index():
         }
     ]
     return render_template('index.html', title='home', user=user, posts=posts)
+
+@flask_app.route('/login')
+def login():
+	form = LoginForm()
+	return render_template('login.html', title='Sign In', form=form)
+
