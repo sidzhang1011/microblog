83f88389c437d87e7f9f2616a367b76c15923b1a
Sid Zhang <zsg@outlook.com>
2020-08-13 22:50:17 +0800

    complete for templates

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import flask_app
 from app.forms import LoginForm
 
@@ -23,8 +23,14 @@ def index():
     ]
     return render_template('index.html', title='home', user=user, posts=posts)
 
-@flask_app.route('/login')
+@flask_app.route('/login', methods=['GET', 'POST'])
 def login():
 	form = LoginForm()
+	if form.validate_on_submit():
+		msg = 'Login requested for user {}, remember_me={}'.format(
+			form.username.data, form.remember_me.data)
+		print(msg)
+		flash(msg)
+		return redirect(url_for('index'))
 	return render_template('login.html', title='Sign In', form=form)
 
