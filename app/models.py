c941a262e2c92a444fcab59d600b9cb5507749a9
Sid Zhang <zsg@outlook.com>
2020-08-20 07:52:30 +0800

    add Post class

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 #coding=utf-8
 
+from datetime import datetime
 from app import db
 
 class User(db.Model):
@@ -10,3 +11,12 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index = True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
