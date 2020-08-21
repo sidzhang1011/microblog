8b501f1cba7a1cb93754b3b7d16b4a65a58c6a60
Sid Zhang <zsg@outlook.com>
2020-08-21 22:12:02 +0800

    password hash

--- app/__init__.py
+++ app/__init__.py
@@ -4,11 +4,14 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
+
 
 flask_app = Flask(__name__)
 flask_app.config.from_object(Config)
 #print(flask_app.config)
 db = SQLAlchemy(flask_app)
 migrate = Migrate(flask_app, db)
+login = LoginManager(flask_app)
 
 from app import routes, models
