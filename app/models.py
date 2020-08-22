3bbb4d5a659b0f04e3f413e3629f5dbf50362526
Sid Zhang <zsg@outlook.com>
2020-08-22 08:41:42 +0800

    user loader

--- app/models.py
+++ app/models.py
@@ -1,9 +1,10 @@
 #coding=utf-8
 
 from datetime import datetime
-from app import db
+from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -21,6 +22,12 @@ class User(UserMixin, db.Model):
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
+
+@login.user_loader
+def load_user(id):
+	return User.query.get(int(id))
+
+
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     body = db.Column(db.String(140))
@@ -29,3 +36,5 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+
