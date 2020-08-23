b36575e5f5d4df0830cf925273c093498a23d769
Sid Zhang <zsg@outlook.com>
2020-08-23 12:24:18 +0800

    add register

--- app/routes.py
+++ app/routes.py
@@ -50,3 +50,19 @@ def login():
 def logout():
 	logout_user()
 	return redirect(url_for('index'))
+
+from app import db
+from app.forms import RegistrationForm
+@flask_app.route('/register', methods=['GET', 'POST'])
+def register():
+	if current_user.is_authenticated:
+		return redirect(url_for('index'))
+	form = RegistrationForm()
+	if form.validate_on_submit():
+		user = User(username=form.username.data, email=form.email.data)
+		user.set_password(form.password.data)
+		db.session.add(user)
+		db.session.commit()
+		flash('Congradulation, you are now a registered user!')
+		return redirect(url_for('login'))
+	return render_template('register.html', title='Register', form=form)
