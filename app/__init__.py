f19ec81220add5632a99b4009432a755f30be833
Sid Zhang <zsg@outlook.com>
2023-07-18 10:49:37 +0800

    error handling

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,14 @@
 #coding=utf-8
+import os.path
 
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+import logging
+from logging.handlers import SMTPHandler
+from logging.handlers import RotatingFileHandler
 
 
 flask_app = Flask(__name__)
@@ -15,4 +19,33 @@ migrate = Migrate(flask_app, db)
 login = LoginManager(flask_app)
 login.login_view = 'login'
 
-from app import routes, models
+from app import routes, models, errors
+
+if not flask_app.debug:
+    if flask_app.config['MAIL_SERVER']:
+        auth = None
+        if flask_app.config['MAIL_USERNAME'] or flask_app.config['MAIL_PASSWORD']:
+            auth = (flask_app.config['MAIL_USERNAME'], flask_app.config['MAIL_PASSWORD'])
+        secure = None
+        if flask_app.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(flask_app.config['MAIL_SERVER'], flask_app.config['MAIL_PORT']),
+            fromaddr='no-reply@' + flask_app.config['MAIL_SERVER'],
+            toaddrs=flask_app.config['ADMINS'], subject='Microblog Failure',
+            credentials=auth, secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        flask_app.logger.addHandler(mail_handler)
+
+        flask_app.logger.setLevel(logging.INFO)
+        flask_app.logger.info('Microblog startup')
+
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    flask_app.logger.addHandler(file_handler)
+
+    flask_app.logger.setLevel(logging.INFO)
+    flask_app.logger.info('Microblog startup')
--- app/models.py
+++ app/models.py
@@ -6,6 +6,10 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from hashlib import md5
 
+# followers = db.Table('followers',
+# 	db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+# 	db.Column
+# )
 
 class User(UserMixin, db.Model):
 	id = db.Column(db.Integer, primary_key=True)
